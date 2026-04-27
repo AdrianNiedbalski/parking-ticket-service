ed4551d45c271feca3cb0758e1265c6f47b90acd
User <niedbalskiadrian@gmail.com>
2026-04-27 17:43:02 +0200

    1st commit

---
+++ src/main/java/org/parkingticketservice/parkingticketservice/ParkingTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package org.parkingticketservice.parkingticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingTicketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/parkingticketservice/parkingticketservice/ParkingTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parkingticketservice.parkingticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
