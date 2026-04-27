e55832a96512952fbad1b239a5920268139eadbb
User <niedbalskiadrian@gmail.com>
2026-04-27 17:34:35 +0200

    Add gitignore

---
+++ src/main/java/org/parkingticketservice/parkingticketserivce/ParkingTicketSerivceApplication.java
@@ -0,0 +1,13 @@
+package org.parkingticketservice.parkingticketserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTicketSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingTicketSerivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/parkingticketservice/parkingticketserivce/ParkingTicketSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parkingticketservice.parkingticketserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTicketSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
